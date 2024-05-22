b7c3c700fb042c771b2b50bf029270f0384403ce
Andres Ramirez <aramirez82@iafas.gov.ar>
2024-05-22 09:36:42 -0300

    pedido de datos del socket

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ const io = require("socket.io")(server, {
       credentials: false
     }
   });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5001;
+
+const activeUsers = {}; // Objeto para almacenar usuarios activos
+
 
 app.use(bodyParser.json());
 
@@ -31,6 +34,19 @@ app.post('/webhook', (req, res) => {
   res.status(200).send('Notification received');
 });
 
+app.post('/list', (req, res) => {
+  // Procesar la notificación según tus necesidades
+ 
+  // Enviar la notificación a través del WebSocket
+  io.emit('list', {
+    datef: '2024-05-10T03:00:00.000Z',
+    dates: '2024-05-25'
+  });
+
+  // Responder a la solicitud de Mercado Pago con un código 200 para confirmar la recepción de la notificación
+  res.status(200).send('List ');
+});
+
 // Función para procesar la notificación recibida del webhook
 function processNotification(notification) {
   // Aquí puedes escribir la lógica para procesar la notificación y tomar acciones en consecuencia
@@ -44,5 +60,23 @@ server.listen(PORT, () => {
 
 // Configurar el WebSocket
 io.on('connection', (socket) => {
-  console.log('Client connected');
+  console.log('Un usuario se ha conectado');
+
+  // Añadir el nuevo usuario a la lista de usuarios activos
+  socket.on('user connected', (username) => {
+      activeUsers[username] = socket.id;
+  });
+
+  socket.on('List Return', (carts) => {
+    console.log(carts)
+});
+
+    
+  // Manejar la desconexión del usuario
+  socket.on('disconnect', () => {
+      console.log('Un usuario se ha desconectado');
+      delete activeUsers[socket.id]; // Eliminar el usuario de la lista de activos
+      io.emit('active users', Object.values(activeUsers)); // Enviar la lista actualizada a todos
+  });
+
 });
